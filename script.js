502f5b901a1ff8330996eed9fc811c7cc56d933e
ixdit <ixdit1979@gmail.com>
2021-04-09 09:26:49 +0300

    lesson 2: part1

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 1000, //Доход за месяц
+    income = 'фриланс', //доход
+    addExpenses = 'интернет, такси, коммуналка', //расходы
+    deposit = false, //накопления
+    mission = 10000, //цель
+    period = 9; //период
+
+let budgetDay = money / 30;
+
 
 alert ('ok');
 
-console.log( 'messege' );
+console.log( typeof (money) );
+console.log( typeof (income) );
+console.log( typeof (deposit) );
+
+console.log( budgetDay );
